5fcd081334ad5552b98cb46cd3b63bf60ff2f6b7
Elizabeth Zachary <elizabethzachary96@gmail.com>
2024-02-09 18:16:02 -0500

    Render test items in PackingList

--- src/components/PackingList.tsx
+++ src/components/PackingList.tsx
@@ -1,3 +1,26 @@
+import Item from "./Item";
+
+const initialItems = [
+  {
+    id: 1,
+    description: "toothbrush",
+    quantity: 1,
+    packed: false
+  },
+  {
+    id: 2,
+    description: "deodorant",
+    quantity: 1,
+    packed: false
+  }
+]
+
 export default function PackingList(): JSX.Element {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map(item =><Item item={item}/>)}
+      </ul>
+    </div>
+    );
 }
