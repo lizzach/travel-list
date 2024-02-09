7c2319fb3558cf9377059442843bafb471a66c72
Elizabeth Zachary <elizabethzachary96@gmail.com>
2024-02-09 17:39:29 -0500

    Add classnames to Form and Stats

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,7 @@
-export default function Form() {
-    return (
-        <div>
-            <h3>What do you need for your trip?</h3>
-        </div>
-    );
+export default function Form(): JSX.Element {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
 }
--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,6 +1,6 @@
 export default function Stats(): JSX.Element {
   return (
-    <footer>
+    <footer className="stats">
         <em>You have X items on your list, and you already packed X%</em>
     </footer>)
 }
