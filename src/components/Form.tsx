24ebe755185765944902f3262165e165b1111d89
Elizabeth Zachary <elizabethzachary96@gmail.com>
2024-02-11 14:04:58 -0500

    add select dropdown to Form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,13 @@
 export default function Form(): JSX.Element {
   return (
-    <div className="add-form">
+    <form className="add-form">
       <h3>What do you need for your trip?</h3>
-    </div>
+      <select>
+        {Array.from({length: 20}, (_, i) => i + 1).map((num) =>
+        <option value={num} key={num}>
+          {num}
+        </option>)}
+      </select>
+    </form>
   );
 }
