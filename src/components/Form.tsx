0c5d2a2fc506f6ce27c24317bef13c602314714b
Elizabeth Zachary <elizabethzachary96@gmail.com>
2024-02-12 13:52:11 -0500

    Add text input and add description and quantity states

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
+
 export default function Form(): JSX.Element {
+
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   return (
     <form className="add-form">
       <h3>What do you need for your trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({length: 20}, (_, i) => i + 1).map((num) =>
-        <option value={num} key={num}>
-          {num}
-        </option>)}
+          <option value={num} key={num}>
+            {num}
+          </option>)}
       </select>
+      <input 
+        type="text" 
+        placeholder="item"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      >
+        
+      </input>
     </form>
   );
 }
